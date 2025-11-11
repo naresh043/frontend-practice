861d18f58ab9de9e7be88c79ba2c29502776854f
NareshSanjeev <naresh732003@gmail.com>
2025-11-11 08:40:42 +0530

    Add findPair function to locate indices of two numbers that sum to a target

--- arrayMethods.js
+++ arrayMethods.js
@@ -31,3 +31,14 @@ const removeFalsy = (arr) => {
 };
 array = [NaN, 0, 15, false, -22, "", undefined, 47, null];
 console.log(removeFalsy(array));
+
+const findPair = (numbers, target) => {
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+};
+console.log(findPair([10, 20, 10, 40, 50, 60, 70], 50));
